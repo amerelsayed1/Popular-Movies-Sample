7d08e72cf1171d3f443e21a63be18b8e0486f064
amerelsayed2020 <amer.elsayed@makeorder.com>
2023-08-16 14:19:53 +0300

    remove unused imports

--- app/src/main/java/com/iamer/movies/features/movies/presentation/MoviesHomeActivity.kt
+++ app/src/main/java/com/iamer/movies/features/movies/presentation/MoviesHomeActivity.kt
@@ -1,42 +1,9 @@
 package com.iamer.movies.features.movies.presentation
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.iamer.movies.R
-import com.iamer.movies.core.extension.observe
-import com.iamer.movies.core.network.ResponseBase
-import com.iamer.movies.core.network.usecase.DataState
-import com.iamer.movies.core.platform.BaseActivity
-import com.iamer.movies.databinding.ActivityMainBinding
-import com.iamer.movies.features.movies.data.models.Movie
-import com.iamer.movies.features.movies.data.models.MoviesResponse
 import com.iamer.movies.ui.theme.MostPopularMoviesSampleTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -49,72 +16,9 @@ class MoviesHomeActivity : ComponentActivity() {
         setContent {
             MostPopularMoviesSampleTheme {
                 moviesHomeMoviesViewModel.getMovies()
-                HomeScreen(moviesHomeMoviesViewModel)
+                MoviesHomeScreen(moviesHomeMoviesViewModel)
             }
         }
-    }
-
-    /*private val moviesAdapter: MoviesAdapter by lazy(LazyThreadSafetyMode.PUBLICATION) {
-        MoviesAdapter()
-    }
-
-    private lateinit var composeView: ComposeView*/
-
-    //override fun viewBinding() = ActivityMainBinding.inflate(layoutInflater)
-
-    /*override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        composeView = ComposeView(applicationContext)
-
-
-        setContent {
-            FirstScreen()
-        }
-
-        *//*setRvMovie()
-
-        moviesHomeMoviesViewModel.getMovies()
-
-        with(moviesHomeMoviesViewModel) {
-            observe(movies, ::renderMoviesList)
-        }
-*//*
-
-    }*/
-
-    private fun setRvMovie() {
-        /* viewBinding?.rvMovies?.apply {
-             val rvMenuItemLayoutManager =
-                 LinearLayoutManager(this.context, RecyclerView.VERTICAL, false)
-             layoutManager = rvMenuItemLayoutManager
-             adapter = moviesAdapter
-         }
-
-         moviesAdapter.clickListener = { movie ->
-             Intent(this@MoviesHomeActivity, MoviesDetailsActivity::class.java).apply {
-                 putExtra("movieId", movie?.id)
-                 startActivity(this)
-             }
-         }*/
-    }
-
-    private fun renderMoviesList(status: DataState<MoviesResponse>) {
-        /*when (status) {
-
-            is DataState.Loading -> {
-                viewBinding?.progressBarLoading?.visibility = View.VISIBLE
-            }
-
-            is DataState.Success -> {
-                viewBinding?.progressBarLoading?.visibility = View.GONE
-                moviesAdapter.submitList(status.response.data)
-            }
-
-            is DataState.Failure -> {
-                viewBinding?.progressBarLoading?.visibility = View.GONE
-            }
-
-        }*/
     }
 
 }
--- app/src/main/java/com/iamer/movies/features/movies/presentation/MoviesHomeScreen.kt
+++ app/src/main/java/com/iamer/movies/features/movies/presentation/MoviesHomeScreen.kt
@@ -1,11 +1,10 @@
 package com.iamer.movies.features.movies.presentation
 
-import android.util.Log
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -13,17 +12,17 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -35,11 +34,10 @@ import com.bumptech.glide.integration.compose.GlideImage
 import com.iamer.movies.R
 import com.iamer.movies.core.network.usecase.DataState
 import com.iamer.movies.features.movies.data.models.Movie
-import com.iamer.movies.features.movies.data.models.MoviesResponse
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(homeMoviesViewModel: HomeMoviesViewModel) {
+fun MoviesHomeScreen(homeMoviesViewModel: HomeMoviesViewModel) {
 
     val result = homeMoviesViewModel.movies.observeAsState()
 
@@ -61,7 +59,13 @@ fun HomeScreen(homeMoviesViewModel: HomeMoviesViewModel) {
             Box(modifier = Modifier.padding(innerPadding)) {
                 when (val response = result.value) {
                     is DataState.Loading -> {
-
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            CircularProgressIndicator()
+                        }
                     }
 
                     is DataState.Success -> {
@@ -81,32 +85,11 @@ fun HomeScreen(homeMoviesViewModel: HomeMoviesViewModel) {
     )
 }
 
-private fun renderMoviesList(status: DataState<MoviesResponse>) {
-    when (status) {
-
-        is DataState.Loading -> {
-
-        }
-
-        is DataState.Success -> {
-
-        }
-
-        is DataState.Failure -> {
-
-        }
-
-    }
-}
-
 @Composable
 fun RecyclerView(
     movies: List<Movie>
 ) {
-    LazyColumn(
-
-    ) {
-
+    LazyColumn {
         items(items = movies) { movie ->
             ItemListMovie(movie)
         }
